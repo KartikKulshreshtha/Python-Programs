55c778bef83285593c5a21fc587f3c7977245699
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2021-01-18 10:56:15 +0530

    Update Your Age in 2090.py

--- Your Age in 2090.py
+++ Your Age in 2090.py
@@ -2,7 +2,10 @@ Age = int(input("Enter your Current Age"))
 
 Year = int(input("And enter the current year"))
 
-while Age<=100:
-    Year = Year + 1
-    Age = Age + 1
+if Year>2025:
+    print('You are not born yet!!!')
+else:
+    while Age <= 100:
+        Year = Year + 1
+        Age = Age + 1
 print(f"You get the age of 100 years in the year : {Year}")
