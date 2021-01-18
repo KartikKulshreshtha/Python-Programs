b7cc0a9f38a2c5c50d71f41457fe8df228c3e066
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2021-01-18 10:49:00 +0530

    Add files via upload

---
+++ Your Age in 2090.py
@@ -0,0 +1,8 @@
+Age = int(input("Enter your Current Age"))
+
+Year = int(input("And enter the current year"))
+
+while Age<=100:
+    Year = Year + 1
+    Age = Age + 1
+print(f"You get the age of 100 years in the year : {Year}")
