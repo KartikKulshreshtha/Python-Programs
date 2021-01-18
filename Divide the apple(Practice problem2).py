0d811481a9432b9366afb510389c115e5b80bbec
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2021-01-18 12:02:14 +0530

    Update Divide the apple(Practice problem2).py

--- Divide the apple(Practice problem2).py
+++ Divide the apple(Practice problem2).py
@@ -1,19 +1,25 @@
-n = int(input("Enter the numbers of the apple harry got : "))
-mn = int(input("Enter the minimum range"))
-mx = int(input("Enter the maximum range"))
+while True:
+    try:
+        n = int(input("Enter the number of the apple harry got"))
+        mn = int(input("Enter the minimum range"))
+        mx = int(input("Enter the maximum range"))
+    except Exception as e:
+        print("You entered a invalid number")
+        exit()
 
+    if mn == mx:
+        print("This is not a range and here mn is equal to mx")
+    else:
 
-if mn == mx:
-    print("This is not a range and here mn is equal to mx")
-else:
+        while mn <= mx:
+            if n % mn == 0:
+                print(f"{mn} is divisible by {n}")
+                mn = mn + 1
+            else:
+                print(f"{mn} is not divisible by {n}")
+                mn = mn + 1
 
-    while mn <= mx:
-        if n % mn == 0:
-            print(f"{mn} is divisible by {n}")
-            mn = mn + 1
-        else:
-            print(f"{mn} is not divisible by {n}")
-            mn = mn + 1
+    break
 
 
 
